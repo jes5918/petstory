6911a2d51684f6aa0f196fcccdbbc4fe35bbd32a
유진우 <phoenix9373@naver.com>
2021-02-08 16:05:22 +0900

    S04P12B204-58 [feat] : FE - 피드 MUI 적용

--- frontend/pet/src/views/Board/Create.jsx
+++ frontend/pet/src/views/Board/Create.jsx
@@ -1,9 +1,9 @@
-import React from "react";
-import ImageUploading from "react-images-uploading";
-import upload from "../../assets/upload.PNG";
+import React from 'react';
+import ImageUploading from 'react-images-uploading';
+import upload from '../../assets/upload.PNG';
 // import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
-import "./Create.css";
+import './Create.css';
 import axios from 'axios';
 
 export default function Create() {
@@ -28,10 +28,11 @@ export default function Create() {
     };
     files.append('title', request.title);
     files.append('context', request.context);
-    axios.post("/api/board/createV2", files, {})
-      .then(response => console.log(response))
-      .then(result => console.log(result))
-      .catch(error => console.error('error', error));
+    axios
+      .post('/api/board/createV2', files, {})
+      .then((response) => console.log(response))
+      .then((result) => console.log(result))
+      .catch((error) => console.error('error', error));
     titleRef.current.value = '';
     contextRef.current.value = '';
     setImages('');
@@ -56,38 +57,33 @@ export default function Create() {
           dragProps,
         }) => (
           // write your building UI
-          <div
-          {...dragProps}>
-            <div
-              className="uploadimg"
-              onClick={onImageUpload}
-            >
-              <img src={upload} alt="왜"/>
-
+          <div {...dragProps}>
+            <div className="uploadimg" onClick={onImageUpload}>
+              <img src={upload} alt="왜" />
             </div>
             <div onClick={onImageRemoveAll}>RESET</div>
             &nbsp;
             <Grid container spacing={1}>
-            {imageList.map((img, index) => (
-              <Grid key={index} className="image-item">
-               <img src={img.data_url} alt="어디써" width="100" />
-                <div className="image-item__btn-wrapper">
-                  <button onClick={() => onImageUpdate(index)}><i className="fas fa-pen color-yellow"></i></button>
-                  <button onClick={() => onImageRemove(index)}><i className="fas fa-times-circle color-red"></i></button>
-                </div>
-              </Grid>
-            ))}
+              {imageList.map((img, index) => (
+                <Grid key={index} className="image-item">
+                  <img src={img.data_url} alt="어디써" width="100" />
+                  <div className="image-item__btn-wrapper">
+                    <button onClick={() => onImageUpdate(index)}>
+                      <i className="fas fa-pen color-yellow"></i>
+                    </button>
+                    <button onClick={() => onImageRemove(index)}>
+                      <i className="fas fa-times-circle color-red"></i>
+                    </button>
+                  </div>
+                </Grid>
+              ))}
             </Grid>
-
           </div>
-
         )}
-
       </ImageUploading>
       <div className="text-div">
-          <input ref={titleRef} type="text" placeholder="제목을 입력하세요"/>
-          <input ref={contextRef} type="text" placeholder="내용을 입력하세요"/>
-
+        <input ref={titleRef} type="text" placeholder="제목을 입력하세요" />
+        <input ref={contextRef} type="text" placeholder="내용을 입력하세요" />
       </div>
       <button>생성하기</button>
     </form>
