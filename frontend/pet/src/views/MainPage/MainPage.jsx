13ccd189d8f25284c98ad74fad2d9f4ec6ed7705
유진우 <phoenix9373@naver.com>
2021-02-08 06:52:53 +0000

    Revert "Merge branch 'feature/feed-menus' into 'develop'"
    This reverts merge request !31

--- frontend/pet/src/views/MainPage/MainPage.jsx
+++ frontend/pet/src/views/MainPage/MainPage.jsx
@@ -1,15 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { withRouter } from 'react-router-dom';
 import FeedDetail from '../../components/Feed/FeedDetail';
+import FeedFrame from '../../components/Feed/FeedFrame';
 import FeedInfinite from '../../components/Feed/FeedInfinite';
-import FeedTest from '../../components/Feed/FeedTest';
+import FeedInfiniteCopy from '../../components/Feed/FeedInfiniteCopy';
 
 function MainPage(props) {
   return (
     <div>
+      {/* <FeedFrame /> */}
       {/* <FeedDetail /> */}
       <FeedInfinite />
-      {/* <FeedTest /> */}
     </div>
   );
 }
