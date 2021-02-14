802c51d7ef2ff7e09c38b3386867343bcdfa1d7a
유진우 <phoenix9373@naver.com>
2021-02-14 13:54:00 +0900

    [누락부분 추가] app.jsx -> SelectProfileModal

--- frontend/pet/src/app.jsx
+++ frontend/pet/src/app.jsx
@@ -14,6 +14,7 @@ import PageNotFound from './views/PageNotFound/PageNotFound';
 import CarToonize from './views/Cartoonize/CartoonizePage';
 import UserDetail from './views/Accounts/UserDetailPage';
 import DetailPage from './views/DetailPage/DetailPage';
+import SelectProfileModal from './views/Profile/SelectProfileModal';
 
 // Component Load
 import NavBar from './components/NavBar/NavBar';
